b962a9f6a4d18568fc81f5798c9ce4f1325dc79c
Biryukov239 <gregory239@mail.ru>
2022-11-28 21:08:59 +0300

    the beginning of personage

---
+++ Personage.py
@@ -0,0 +1,26 @@
+import pygame as pg
+
+
+class Personage:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.Vx = 0
+        self.Vy = 0
+        self.m = 10
+        self.color = "red"
+        self.Fy = 0
+        self.radius = 40
+
+    def interaction_with_keyboard(self, Vy, Vx):
+        self.Vx = Vx
+        self.Vy = Vy
+        return 0
+
+    def draw(self, screen, x, y):
+        pg.draw.circle(screen, self.color, x, y, self.radius)
+
+    def move(self, x, y):
+        x += self.Vx
+        y += self.Vy
+        return x, y
