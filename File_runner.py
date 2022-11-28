c0ad32c1cfc5f4c185edf2c81cd1c6bb16e95394
Biryukov239 <gregory239@mail.ru>
2022-11-28 21:51:11 +0300

    first draw without move

--- File_runner.py
+++ File_runner.py
@@ -1,10 +1,11 @@
 import pygame as pg
 from pygame.draw import *
-
+from pynput.mouse import Controller
 from Personage import *
 from level_constructor import *
 
 pg.init()
+mouse = Controller()
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 running = True
 block = Block(screen)
@@ -14,7 +15,7 @@ map_list = read_map(map_name)
 while running:
     screen.fill(TIME_COLOR)
     draw_map(block, map_list)
-    p.draw(screen, 100, 50)
+    p.draw(screen, mouse.click[0], mouse.position[1])
     pg.display.update()
     for event in pg.event.get():
         if event.type == pg.QUIT:
